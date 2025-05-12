de83efe3a9bf5e0c92f015fa786a55db02ad3748
Vasudha Meka <112605177+VasudhaMeka@users.noreply.github.com>
2025-05-12 11:30:48 +0530

    Add files via upload

---
+++ sql_queries/churn_analysis.sql
@@ -0,0 +1,90 @@
+use customerchurnanalysis;
+
+-- Basic Exploration
+select * from customers;
+
+-- Check nulls
+select count(*) - count(total_charges) as missing_total_charges
+from customers;
+
+-- Churn Overview
+-- 1. Total Churn Count
+select churn_label, count(*) as total
+from customers
+group by churn_label;
+
+-- 2. Churn Rate
+select round(
+sum(
+	case when churn_label = 'Yes' then 1
+    else 0
+    end
+	) * 100 / count(*) , 2
+) as churn_rate
+from customers;
+
+-- Churn Analysis by Key Factors
+-- 1. By Contract Type
+select contract, count(*) as total_customers,
+sum(
+		case 	
+		when churn_label = 'Yes' then 1
+		else 0
+		end
+	) as churned,
+round(sum(
+	case
+    when churn_label = 'Yes' then 1
+    else 0
+    end
+	) * 100 / count(*),2) as churn_rate
+    from customers
+    group by contract;
+    
+-- 2. By Tenure Range
+select
+	case
+		when tenure_months between 0 and 12 then '0-12 months'
+        when tenure_months between 13 and 24 then '13-24 months'
+        when tenure_months between 25 and 48 then '25-48 months'
+        else '49+ months'
+	end as tenure_group,
+    count(*) as total,
+    sum(case when churn_label = 'Yes' then 1 else 0 end) as churned,
+    round(sum(case
+				when churn_label = 'Yes' then 1 else 0 end) * 100 / count(*), 2) as churn_rate
+from customers
+group by tenure_group;
+
+-- 3. By Internet Service
+select internet_service, count(*) as total,
+sum(case when churn_label = 'Yes' then 1 else 0 end) as churned
+from customers
+group by internet_service;
+
+-- 3. By Senior Citizen
+select senior_citizen, count(*) as total,
+sum(case when churn_label = 'Yes' then 1 else 0 end) as churn_rate
+from customers
+group by senior_citizen;
+
+-- Monthly Charges & CLTV
+-- 1. Average Monthly Charges vs Churn
+select churn_label, round(avg(monthly_charges),2) as avg_monthly_charges
+from customers
+group by churn_label;
+
+-- Average CLTV vs Churn
+select churn_label, round(avg(cltv),2) as avg_cltv
+from customers
+group by churn_label;
+
+-- Geographic Patterns
+-- 1. Top Cities by Churn Count
+select city, count(*) as churned
+from customers
+where churn_label = 'Yes'
+group by city
+order by churned desc
+limit 10;
+
--- sql_queries/table_creation.sql
+++ sql_queries/table_creation.sql
@@ -1,1 +1,38 @@
+use customerchurnanalysis;
+
+create table Customers (
+customerID varchar(20),
+count int,
+country varchar(20),
+state varchar(20),
+city varchar(20),
+zip_code varchar(10),
+lat_long varchar(100),
+latitude decimal(9,6),
+longitude decimal(9,6),
+gender varchar(10),
+senior_citizen varchar(10),
+partner varchar(10),
+dependents varchar(10),
+tenure_months int,
+phone_service varchar(10),
+multiple_lines varchar(10),
+internet_service varchar(50),
+online_security varchar(10),
+online_backup varchar(10),
+device_protection varchar(10),
+tech_support varchar(10),
+streaming_tv varchar(10),
+streaming_movies varchar(10),
+contract varchar(50),
+paperless_billing varchar(10),
+payment_method varchar(50),
+monthly_charges decimal(10,2),
+total_charges decimal(10,2),
+churn_label varchar(10),
+churn_value int,
+churn_score int,
+cltv int,
+churn_reason varchar(100)
+);
 
